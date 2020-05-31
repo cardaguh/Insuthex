94aa7e63d41962c7ea57b97b8454c6bf615253ac
jvalderrama16 <jcamilo.valderrama@udea.edu.co>
2020-05-30 19:00:49 -0500

    agregada pantalla de perfiles

---
+++ app/src/main/java/co/fashionconnect/hackaton/Perfiles.java
@@ -0,0 +1,14 @@
+package co.fashionconnect.hackaton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Perfiles extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfiles);
+    }
+}
